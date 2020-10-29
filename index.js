bfdcfb99bb832b3e8bba20a516b8030db3e4b1f1
Modulo <modzero@protonmail.com>
2020-10-29 15:34:40 -0500

    Create services
    Initialize services and provide /info endpoint

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+const config = require('./config.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+// Initialize services
+console.log('Initializing ' + config.service_quantity + ' services...');
+var services = [];
+for (let i = 0; i < config.service_quantity; i++) {
+  services.push({
+    popularity: Math.random()
+  });
+}
+console.log(config.service_quantity + ' services initialized.' + "\n");
+
+// Create endpoints
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.get('/info', (req, res) => {
   return res.status(200).send({
-  	message: 'hello'
+    services: services
   });
 });
 
-app.listen(4000);
+// Start server
+console.log('Starting server...');
+app.listen(config.port, () => {
+  console.log('Server running on port ' + config.port + '.');
+});
