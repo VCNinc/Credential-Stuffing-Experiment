f7044e96731abaff500edea9e18b02d1afab08e2
Modulo <modzero@protonmail.com>
2020-10-29 16:03:17 -0500

    Single-service model
    Switch to single-service model with login/register features

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-const config = require('./config.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const port = process.env.PORT || 4000;
 
-// Initialize services
-console.log('Initializing ' + config.service_quantity + ' services...');
-var services = [];
-for (let i = 0; i < config.service_quantity; i++) {
-  services.push({
-    popularity: Math.random()
-  });
-}
-console.log(config.service_quantity + ' services initialized.' + "\n");
+const popularity = Math.random();
+var users = {};
 
-// Create endpoints
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.get('/info', (req, res) => {
   return res.status(200).send({
-    services: services
+    popularity: popularity
   });
 });
 
-// Start server
+app.post('/register', (req, res) => {
+  users[req.body.username] = req.body.password;
+  console.log('Successful registration (' + req.body.username + ':' + req.body.password + ')');
+  return res.status(200).send();
+});
+
+app.post('/login', (req, res) => {
+  if (users[req.body.username] === req.body.password) {
+    console.log('Successful login (' + req.body.username + ':' + req.body.password + ')');
+    return res.status(200).send({success: true});
+  } else {
+    console.log('Failed login (' + req.body.username + ':' + req.body.password + ')');
+    return res.status(200).send({success: false});
+  }
+});
+
 console.log('Starting server...');
-app.listen(config.port, () => {
-  console.log('Server running on port ' + config.port + '.');
+app.listen(port, () => {
+  console.log('Server running on port ' + port + '.');
 });
